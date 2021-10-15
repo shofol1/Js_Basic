a89e5708fd937c8eabab813bd03cf7c53d425e4a
Md Ashikul Hossain Shfol <63502805+shofol1@users.noreply.github.com>
2021-10-15 22:08:10 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const button=document.getElementById('btn');
+const counterField=document.getElementById('counter-field');
+const button1=document.getElementById('btn1');
+const inputField=document.getElementById('input-field');
+const inputPrinter=document.getElementById('inputPrinter');
+//htnl tag re ekta id dewa
+//ekta variable sei id takee rakha/dhoraa
+//event add korte chai event add koraa
+// button.addEventListener('click',function(){
+//     counterField.innerText++;
+// });
+
+// // button1.addEventListener('click',function(){
+// //     // const inputValue=inputField.value;
+// //     // console.log(inputValue);
+// //     inputPrinter.innerText=inputField.value ;
+// //     inputField.value="";
+// // });
+// inputField.addEventListener('keyup',function(){
+//     inputPrinter.innerText=inputField.value ;
+// })
